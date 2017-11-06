0ad00cea3c8a66f3c0fbf8376ef18b7dd4d781e1
CamilleINGOUF <ingouf.camille@gmail.com>
2017-11-06 14:42:08 +0100

    init

---
+++ src/sdes.java
@@ -0,0 +1,20 @@
+import java.util.BitSet;
+
+public class sdes 
+{
+	public static BitSet[] createKeys(BitSet K)
+	{
+		BitSet[] ks = new BitSet[2];
+		
+		
+		
+		return ks;
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		BitSet k = new BitSet();
+		
+	}
+}
